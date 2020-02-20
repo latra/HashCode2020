88e233bcd0c45bc2f56aa96f9e541909adfc656a
alamas <alamas@minsait.com>
2020-02-20 20:01:48 +0100

    spanish WTF???

--- book.py
+++ book.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 class Book:
-    def __init__(self, book_id, puntuacion):
+    def __init__(self, book_id, punctuation):
         self.book_id = book_id
-        self.puntuacion = puntuacion
+        self.punctuation = punctuation
     def __eq__(self, other):
-        return self.puntuacion == other.puntuacion
+        return self.punctuation == other.punctuation
     def __lt__(self, other):
-        return self.puntuacion < other.puntuacion
+        return self.punctuation < other.punctuation
     def __gt__(self, other):
-        return self.puntuacion > other.puntuacion
+        return self.punctuation > other.punctuation
     def __le__(self, other):
-        return self.puntuacion <= other.puntuacion
+        return self.punctuation <= other.punctuation
     def __ge__(self, other):
-        return self.puntuacion >= other.puntuacion
+        return self.punctuation >= other.punctuation
