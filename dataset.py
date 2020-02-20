aa1b57e5b5044e4598e275ef6e63a09cab4c8a10
Jofre <jofre@google.com>
2020-02-20 20:41:32 +0100

    stuff

--- dataset.py
+++ dataset.py
@@ -6,9 +6,7 @@ from library import Library
 class Dataset:
   def __init__(self,URL):
     raw_values = re.split(r'\n',requests.get(URL).text)[:-1]
-    print(raw_values)
     dataset_globals = re.split(r'\s', raw_values.pop(0))
-    print(dataset_globals)
     self.total_books = int(dataset_globals.pop(0))
     self.total_libraries = int(dataset_globals.pop(0))
     self.total_days = int(dataset_globals.pop(0))
@@ -18,8 +16,6 @@ class Dataset:
     for book_value in re.split(r'\s', raw_values.pop(0)):
         self.books.append(Book(book_id, int(book_value)))
         book_id += 1
-    for book in self.books:
-      print(book)
     self.libraries = []
     for library in range(self.total_libraries):
       library_data = re.split(r'\s', raw_values.pop(0))
