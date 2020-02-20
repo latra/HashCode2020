98a4a070d24c9a24589806fa7eb048384777d38e
alamas <alamas@minsait.com>
2020-02-20 21:03:59 +0100

    solve my shit

--- test_library.py
+++ test_library.py
@@ -3,7 +3,7 @@ from book import Book
 
 def main():
     library = Library(1, [Book(0, 3), Book(1,5)],2, 1, 2)
-    library.print_books()
+    print(library.get_punctuation(10, []))
     pass
 
 if __name__ == "__main__":
