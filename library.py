97474ff3dcd7ac7ae1d581f77b16b179e37a3d05
Latra Gallucci <personadelmonton@gmail.com>
2020-02-20 20:43:29 +0100

    Actualizacion de tests

--- library.py
+++ library.py
@@ -17,17 +17,17 @@ class Library:
 
     def get_punctuation(self, availible_time, scanned_books):
         punctuation = 0
-        for i in math.ceil(int(len(self.books)/self.books_in_parallel)):
+        for i in range (math.ceil(int(len(self.books)/self.books_in_parallel))):
             if(i==availible_time):
                 break
             position = i*self.books_in_parallel
-            if position+self.books_in_parallel < len(books):
-                punctuation = puctuation + self.sum_punctuations(self.books[position], scanned_books)
+            if position+self.books_in_parallel < len(self.books):
+                punctuation = punctuation + self.sum_punctuations(self.books[position], scanned_books)
         return punctuation
 
     def sum_punctuations(self, books, scanned_books):
-        puctuation = 0
+        punctuation = 0
         books = list(set(books)-set(scanned_books))
         for book in books:
-            puctuation = puntuation + book.punctuation
+            punctuation = punctuation + book.punctuation
         return punctuation
