5205b925a488c291285fbd1ad0ed963dd44b3468
Jofre <jofre@google.com>
2020-02-20 20:51:02 +0100

    properly asserting types in books and libraries (specially ints)

--- library.py
+++ library.py
@@ -4,12 +4,12 @@ from book import Book
 
 class Library:
     def __init__(self, library_id, books, total_books, books_in_parallel, time):
-        self.id = library_id
-        self.books = books
+        self.id = int(library_id)
+        self.books = int(books)
         self.books.sort(key=lambda x: x.punctuation, reverse=True)
-        self.total_books = total_books
-        self.books_in_parallel = books_in_parallel
-        self.time = time
+        self.total_books = int(total_books)
+        self.books_in_parallel = int(books_in_parallel)
+        self.time = int(time)
 
     def print_books(self):
         for book in self.books:
